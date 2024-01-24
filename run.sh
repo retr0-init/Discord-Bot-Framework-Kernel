86c7249424ea6e487e53304a3cdc6b041c76673f
retr0-init <146389702+retr0-init@users.noreply.github.com>
2024-01-24 22:48:29 +0000

    Python Download command rate limitation Fixes #23

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ pushd ${CURRENT_DIR}
 python3 -m venv venv
 venv/bin/pip install -r requirements.txt
 
-firejail --profile=$(pwd)/firejail.profile --read-write=$(pwd) --blacklist=$(pwd)/firejail.profile --noexec=$(pwd)/extensions venv/bin/python main.py
+firejail --profile=$(pwd)/firejail.profile --read-write=$(pwd) --read-only=$(pwd)/firejail.profile --noexec=$(pwd)/extensions venv/bin/python main.py
 
 popd
